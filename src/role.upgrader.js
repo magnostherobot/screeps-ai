729274737f300e0a473c2b0e09255d7e727f81f9
Tom Harley <magtherob@gmail.com>
2018-06-26 07:28:47 +0100

    Upgrade upgraders

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -1,14 +1,20 @@
 module.exports = {
   run: (creep) => {
-    if (creep.carry.energy < creep.carryCapacity) {
+    if (creep.memory.state === "withdrawing") {
       const source = Game.spawns['Initial'];
       if (creep.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         creep.moveTo(source);
+      }
+      if (creep.carry.energy == creep.carryCapacity) {
+        creep.memory.state = "upgrading";
       }
     } else {
       const controller = creep.room.controller;
       if (creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
         creep.moveTo(controller, { range: 3 });
+      }
+      if (creep.carry.energy == 0) {
+        creep.memory.state = "withdrawing";
       }
     }
   }
